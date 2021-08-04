7b9e931b84e2741896e0cae1256fef6c3aa50a0b
sanjoli63 <sanjoligo6mar@gmail.com>
2021-08-04 15:41:11 +0530

    get all end points

--- lib/app/repositries/data_repository.dart
+++ lib/app/repositries/data_repository.dart
@@ -1,3 +1,4 @@
+import 'package:covid_app/app/repositries/endpoints_data.dart';
 import 'package:covid_app/app/services/api.dart';
 import 'package:covid_app/app/services/api_services.dart';
 import 'package:http/http.dart';
@@ -24,5 +25,27 @@ class DataRepository {
       }
       rethrow;
     }
+  }
+
+  Future<EndpointsData> _getAllEndpointData() async {
+    final values = await Future.wait([
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.cases),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.casesSuspected),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.casesConfirmed),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.deaths),
+      apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: Endpoint.recovered),
+    ]);
+    return EndpointsData(values: {
+      Endpoint.cases: values[0],
+      Endpoint.casesSuspected: values[1],
+      Endpoint.casesConfirmed: values[2],
+      Endpoint.deaths: values[3],
+      Endpoint.recovered: values[4],
+    });
   }
 }
---
+++ lib/app/repositries/endpoints_data.dart
@@ -0,0 +1,15 @@
+class EndpointsData {
+  final int cases;
+  final int casesSuspected;
+  final int casesConfirmed;
+  final int deaths;
+  final int recovered;
+
+  EndpointsData(
+    this.cases,
+    this.casesSuspected,
+    this.casesConfirmed,
+    this.deaths,
+    this.recovered,
+  );
+}
